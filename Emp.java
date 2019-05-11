f21d9286bd44bbc06eca266a6fbd4d56aaad75de
clannad2000 <toby1211@126.com>
2019-05-12 00:01:07 +0800

    init

---
+++ Emp.java
@@ -0,0 +1,5 @@
+public class Emp{
+	public static void main(String args[]){
+		System.out.println("好好学习,天天向上")
+	}
+} 
---
+++ newcs.java
@@ -0,0 +1,4 @@
+public static void main(args[]){
+   System.out.println("123");
+}22121212212
+33333333
